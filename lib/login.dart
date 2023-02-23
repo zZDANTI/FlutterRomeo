b55d60e24911eaa95733b2a701b1a24589f4854f
zZDANTI <Salvadantizz@gmail.com>
2023-02-23 01:29:56 +0100

    ghnghnh

---
+++ lib/login.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+
+// ignore_for_file: prefer_const_constructors
+
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  State<Login> createState() => _LoginPagina();
+}
+
+class _LoginPagina extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+          child: Center(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+          //Hola
+          Text('Hotel Romeo',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 24,
+              )),
+
+          Text('Bienvenido',
+              style: TextStyle(
+                fontSize: 20,
+              )),
+          SizedBox(height: 20),
+
+          //Texto Email
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                border: Border.all(color: Colors.white),
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Email',
+                  ),
+                ),
+              ),
+            ),
+          ),
+
+          //Texto Contraseña
+
+          SizedBox(height: 10),
+
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                border: Border.all(color: Colors.white),
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20.0),
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Contraseña',
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 10),
+
+          // Boton de inicio Sesion
+
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Container(
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                  color: Colors.deepPurple,
+                  borderRadius: BorderRadius.circular(12)),
+              child: Center(
+                child: Text(
+                  'Iniciar Sesión',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 10),
+
+          // No te has registrado
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                '¿Aun no te has registrado? ',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'Registrate aqui',
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          )
+        ]),
+      )),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'login.dart';
+
+// ignore_for_file: prefer_const_constructors
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Login(),
+
+    );
+  }
+}
+
+
+
